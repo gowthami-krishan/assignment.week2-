bd7ec1fa472b0c8ac54aaf63a69a434417eff838
gowthami-krishan <108142949+gowthami-krishan@users.noreply.github.com>
2022-06-24 22:48:23 +0530

    Add files via upload

---
+++ Loginpage.java
@@ -0,0 +1,17 @@
+package Sample1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
