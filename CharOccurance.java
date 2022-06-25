8385a97cc2a75f914b3469b9f5cc478fd866d236
gowthami-krishnan <108142949+gowthami-krishnan@users.noreply.github.com>
2022-06-25 17:21:25 +0530

    Add files via upload

---
+++ CharOccurance.java
@@ -0,0 +1,23 @@
+package classroom;
+
+public class CharOccurance {
+
+	public static void main(String[] args) {
+		
+		String str="welcome to chennai";
+		System.out.println("String: "+str);
+		char[] ch =str.toCharArray();
+	
+	for(int i=0;i<ch.length;i++) {
+		System.out.println(ch[i]+"");
+	}
+		
+		
+	}
+}
+		
+	
+
+	
+
+
